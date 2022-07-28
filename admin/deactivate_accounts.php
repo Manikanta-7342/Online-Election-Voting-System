8f22c5d6e5605e900d428a9bc93187ec0d021307
MANI <maniadishu7342@gmail.com>
2022-07-28 22:47:35 +0530

    Revert "Initial commit"
    This reverts commit 9caef652985d9c8a67852cff5b1bb7b1cdb36eb7.

--- admin/deactivate_accounts.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	require_once 'dbcon.php';						
-	$conn->query("UPDATE voters SET account = 'Inactive'")or die($conn->error);
-	echo "<script> window.location='voters.php' </script>";
-?>			
