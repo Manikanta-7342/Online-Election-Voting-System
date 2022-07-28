d05760fba218a3698e7c0501260419abf10c1cc8
MANI <maniadishu7342@gmail.com>
2022-07-28 22:49:09 +0530

    Revert "Revert "Initial commit""
    This reverts commit 8f22c5d6e5605e900d428a9bc93187ec0d021307.

---
+++ slider.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/slider.css">
+</head>
+<body>
+
+</body>
+</html>
+<div id="slider">
+	<figure>
+<img src="img/banner-1.png">
+<img src="img/banner-2.png">
+<img src="img/banner-3.png">
+<img src="img/banner-1.png">
+<img src="img/banner-2.png">
+    </figure>
+</div>
